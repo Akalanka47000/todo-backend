5c285aa2fa7b14c0947fe3c1340a4472adf759e6
Akalanka Perera <akalankaperera128@gmail.com>
2022-07-02 14:59:09 +0530

    Removed todo related files

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -4,12 +4,8 @@ const router = express.Router()
 
 const healthRouter = require('./health')
 const authRouter = require('./auth')
-const taskRouter = require('./task')
-const statusRouter = require('./status')
 
 router.use('/', healthRouter);
 router.use('/auth', authRouter);
-router.use('/task', taskRouter);
-router.use('/status', statusRouter);
 
 module.exports = router
